05f43766ab782b78ed42c17b084aef311474e09e
Vítor Arimatéa <vitorodev@outlook.com>
2023-01-05 01:35:27 -0300

    Adicionando root e imagem na home

--- src/pages/home/main.js
+++ src/pages/home/main.js
@@ -5,12 +5,12 @@ export default () => {
     container.innerHTML = `
       <section class="page-section">
         <figure class="box">
-          <img class="home-img shadow" src="">
+          <img class="home-img shadow" src="assets/home-ori.png">
         </figure>
         <div class="box">
           <div class="section-text">
-            <h2>Início</h2>
-            <p>Em produção...<p>
+            <h2>Vamos falar sobre essa obra de arte</h2>
+            <p>Ori and the blind forest é um jogo em plataforma, onde você irá se aventurar em cenários mágnificos.<p>
           </div>
         </div> 
       </section>  
