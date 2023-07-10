e29c1ff1df0a746e41406f9cd468e78df54f351d
Vítor Arimatéa <vitorodev@outlook.com>
2023-07-10 16:04:56 -0300

    first commit

--- src/pages/wisps/main.js
+++ src/pages/wisps/main.js
@@ -1,11 +1,11 @@
 export default () => {
-  const container = document.createElement("div")
-  container.classList.add("container")
-
-  container.innerHTML = `
+    const container = document.createElement("div");
+    container.classList.add("container");
+    
+    container.innerHTML = `
       <section class="page_section">
         <figure class="box">
-          <img class="contact-img shadow" src="./assets/credits-naru-ori.png">
+          <img class="about-img shadow" src="./assets/about-ori.png">
         </figure>
         <div class="box">
           <div class="section_text">
@@ -16,8 +16,8 @@ export default () => {
             <p>O título é a sequência do game original Ori and the Blind Forest, lançado em 2015. Neste novo segundo capítulo, os jogadores devem ajudar Ori a encontrar Kun, sua amiga coruja que se perdeu durante um voo.<p>
           </div> 
         </div> 
-      </section>    
+      </section>  
     `
-
-  return container
-}
+  
+    return container; 
+  }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import home from "./pages/home/main.js";
 import credits from "./pages/credits/main.js";
-import about from "./pages/about/main.js";
+import about from "./pages/wisps/main.js";
 
 export default {
   home: home(),
   credits: credits(),
-  about: about()
-}
+  about: about(),
+};
